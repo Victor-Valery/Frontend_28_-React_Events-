e03077e278e281d9b332f2aac6fbc7e1c6d44fe0
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-08-10 19:35:35 +0200

    first commit

---
+++ react-events-main/src/App.js
@@ -0,0 +1,19 @@
+// import EventBind from './components/eventBinding/EventBind'
+// import MouseEvents from './components/mouseEvents/MouseEvents'
+// import KeyboardEvents from './components/keyboardEvents/KeyboardEvents'
+import Form from './components/form/Form'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="App">
+            {/* <h1>Events in React</h1> */}
+            {/* <EventBind /> */}
+            {/* <MouseEvents /> */}
+            {/* <KeyboardEvents /> */}
+            <Form />
+        </div>
+    )
+}
+
+export default App
